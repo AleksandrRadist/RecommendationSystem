fcb8f67f2e5411350bb68b13b5159b540ff9a2c4
Александр Хан <khanaleks2000@gmail.com>
2021-05-20 01:21:44 +0300

    'add_new_api_get_sources'

--- recsystem/api/urls.py
+++ recsystem/api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from .views import (api_orders, api_orders_detail, api_orders_confirm, api_orders_cancel,
                     api_orders_public, api_orders_public_detail, api_orders_complete,
-                    api_orders_accept)
+                    api_orders_accept, api_orders_confirmed, api_orders_accepted,
+                    api_orders_completed)
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 urlpatterns = [
@@ -16,6 +17,9 @@ urlpatterns = [
     path('orders/<str:code>/cancel/', api_orders_cancel, name='api_orders_status_cancel'),
     path('orders/<int:order_id>/complete/', api_orders_complete, name='api_orders_complete'),
     path('orders/<int:order_id>/accept/', api_orders_accept, name='api_orders_accept'),
+    path('orders/confirmed/', api_orders_confirmed, name='api_orders_confirmed'),
+    path('orders/accepted/', api_orders_accepted, name='api_orders_accepted'),
+    path('orders/completed/', api_orders_completed, name='api_orders_completed'),
     path('orders/', api_orders, name='api_orders'),
     path('orders/<str:code>/', api_orders_detail, name='api_orders_detail'),
 ]
--- recsystem/api/views.py
+++ recsystem/api/views.py
@@ -74,9 +74,36 @@ def api_orders(request):
             return Response(OrderSerializer(order).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     if request.method == 'GET':
-        orders = Order.objects.filter(status=True)
+        orders = Order.objects.all()
         serializer = OrderSerializer(orders, many=True)
         return Response(serializer.data)
+
+
+@api_view(['GET'])
+def api_orders_confirmed(request):
+    if not request.user.is_authenticated:
+        return Response(status=status.HTTP_403_FORBIDDEN)
+    orders = Order.objects.filter(confirmation_status=True)
+    serializer = OrderSerializer(orders)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def api_orders_accepted(request):
+    if not request.user.is_authenticated:
+        return Response(status=status.HTTP_403_FORBIDDEN)
+    orders = Order.objects.filter(acceptance_status=True)
+    serializer = OrderSerializer(orders)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def api_orders_completed(request):
+    if not request.user.is_authenticated:
+        return Response(status=status.HTTP_403_FORBIDDEN)
+    orders = Order.objects.filter(completion_status=True)
+    serializer = OrderSerializer(orders)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
