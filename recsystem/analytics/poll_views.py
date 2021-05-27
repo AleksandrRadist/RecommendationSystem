7893d71dd2f39983d3749e75000187daadad8690
Александр Хан <khanaleks2000@gmail.com>
2021-05-28 01:39:26 +0300

    add_commercial_info

--- recsystem/analytics/poll_views.py
+++ recsystem/analytics/poll_views.py
@@ -1,4 +1,4 @@
-from .models import Order
+from .models import Order, CommercialInfo
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from .utils import get_clients_data_age, get_clients_data_gender
@@ -6,11 +6,29 @@ from .utils import get_clients_data_age, get_clients_data_gender
 
 def order_data_gender(request, order_id):
     order = get_object_or_404(Order, id=order_id)
-    data = get_clients_data_gender(order)
+    flag = request.GET.get('clients')
+    if flag == 'performed_action':
+        clients = order.commercial_info.performed_action_clients
+    elif flag == 'shown':
+        clients = order.commercial_info.shown_clients
+    elif flag == 'clicked':
+        clients = order.commercial_info.clicked_clients
+    else:
+        clients = order.clients
+    data = get_clients_data_gender(clients)
     return JsonResponse(data, safe=False)
 
 
 def order_data_age(request, order_id):
     order = get_object_or_404(Order, id=order_id)
-    data = get_clients_data_age(order)
+    flag = request.GET.get('clients')
+    if flag == 'performed_action':
+        clients = order.commercial_info.performed_action_clients
+    elif flag == 'shown':
+        clients = order.commercial_info.shown_clients
+    elif flag == 'clicked':
+        clients = order.commercial_info.clicked_clients
+    else:
+        clients = order.clients
+    data = get_clients_data_age(clients)
     return JsonResponse(data, safe=False)
