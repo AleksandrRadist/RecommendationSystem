249ba48f597d7b4819d513d5207e57409cfa5ff9
knyht <polkarpovkirill@gmail.com>
2021-05-30 21:51:01 +0300

    added module data preprocessing

--- model/final_model/model.py
+++ model/final_model/model.py
@@ -1,5 +1,6 @@
 import psycopg2
-import pandas as pd
+import pandas.io.sql as sqlio
+from data_processing import data_processing
 
 db_connect = psycopg2.connect(
     host='127.0.0.1',
@@ -9,9 +10,19 @@ db_connect = psycopg2.connect(
     password='qweasd'
 )
 
-cursor = db_connect.cursor()
+# cursor = db_connect.cursor()
 
+select_clients_data = 'SELECT * FROM analytics_client'
+select_categories_data = 'SELECT * FROM analytics_category'
 select_transactions_data = 'SELECT * FROM analytics_transaction'
-cursor.execute(select_transactions_data)
-transaction_data = cursor.fetchall()
-print(transaction_data)
+
+# cursor.execute(select_transactions_data)
+# transaction_data = cursor.fetchall()
+clients_data = sqlio.read_sql_query(select_clients_data, db_connect)
+categories_data = sqlio.read_sql_query(select_categories_data, db_connect)
+transaction_data = sqlio.read_sql_query(select_transactions_data, db_connect)
+
+data = data_processing(clients_data, categories_data, transaction_data)
+print(data)
+
+db_connect = None
