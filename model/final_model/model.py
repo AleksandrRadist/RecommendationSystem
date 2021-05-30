6f57b66625beaf55c726d1ab8f19f64b8cb9390e
knyht <kplkrpv@gmail.com>
2021-05-30 21:56:03 +0300

    hm

--- model/final_model/model.py
+++ model/final_model/model.py
@@ -1,5 +1,6 @@
 import psycopg2
 import pandas as pd
+import pandas.io.sql as sqlio
 
 db_connect = psycopg2.connect(
     host='127.0.0.1',
@@ -9,9 +10,11 @@ db_connect = psycopg2.connect(
     password='qweasd'
 )
 
-cursor = db_connect.cursor()
+# cursor = db_connect.cursor()
 
 select_transactions_data = 'SELECT * FROM analytics_transaction'
-cursor.execute(select_transactions_data)
-transaction_data = cursor.fetchall()
+# cursor.execute(select_transactions_data)
+# transaction_data = cursor.fetchall()
+transaction_data = sqlio.read_sql_query(select_transactions_data, db_connect)
+
 print(transaction_data)
