1978535449221f1de5ccc86191c23d2b9e580f1f
knyht <polkarpovkirill@gmail.com>
2021-05-30 19:15:56 +0300

    connect to database

--- model/test2.py
+++ model/test2.py
@@ -1,1 +1,16 @@
-print('hi test')
+import psycopg2
+
+print('Connecting to the PostgreSQL database...')
+db_connect = psycopg2.connect(
+    host='localhost',
+    database='reccomendation_system',
+    user='Aleksandr',
+    password='qweasd'
+)
+
+cursor = db_connect.cursor()
+
+print('PostgreSQL database version:')
+cursor.execute('SELECT version()')
+
+
